b3f82328c39c8c317a7da3605131a975d4c03072
Tomasz <tomasz.urbaniak@me.com>
2017-04-11 19:05:30 +0200

    Fertig

--- Fibbunacci-Zahlenreihe/Program.cs
+++ Fibbunacci-Zahlenreihe/Program.cs
@@ -21,10 +21,10 @@ namespace Fibbunacci_Zahlenreihe
 
         public void Init()
         {
-            fibbunacciHandler = (i) => i == 0 ? 0 : i == 1 ? 1 : fibbunacciHandler(i-2) + fibbunacciHandler(i-1);
+            fibbunacciHandler = (i) => i == 0 ? 0 : i == 1 || i == 2 ? 1 : fibbunacciHandler(i-2) + fibbunacciHandler(i-1);
             // Beispiel: 33 => 3 524 578
             Console.WriteLine(fibbunacciHandler(33));
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
